29c17b3a9534237d3a0ce696e8b64fb39a3611cb
itterative <190138728+itterative@users.noreply.github.com>
2025-09-19 14:45:37 +0000

    feat: retry logic for sessions

--- yadc/captioners/api/session.py
+++ yadc/captioners/api/session.py
@@ -2,25 +2,59 @@ import sys
 import requests
 import functools
 
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+
 from concurrent.futures import ThreadPoolExecutor
 from contextlib import contextmanager
 from urllib.parse import urlparse, ParseResult
 
 
 class Session:
-    def __init__(self, base_url: str, headers: dict[str, str]|None = None):
+    def __init__(
+        self,
+        base_url: str,
+        headers: dict[str, str] | None = None,
+        max_retries: int = 3,
+        backoff_factor: float = 1.0,
+        status_forcelist: tuple[int, ...] = (429, 502, 503, 504),
+    ):
         self.base_url = urlparse(base_url)
         self.headers = headers or {}
 
         self.headers['User-Agent'] = self._user_agent
 
         self._session = requests.Session()
+        self._setup_retries(max_retries, backoff_factor, status_forcelist)
+
         self._pool = ThreadPoolExecutor(max_workers=16, thread_name_prefix='Thread-api-')
 
     @property
     def _user_agent(self):
         import yadc # dependency loop if used at the top
         return f'yadc/{yadc.__version__} (python {sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro})'
+
+    def _setup_retries(
+        self,
+        max_retries: int,
+        backoff_factor: float,
+        status_forcelist: tuple[int, ...]
+    ):
+        retry_strategy = Retry(
+            total=max_retries,
+            read=max_retries,
+            connect=max_retries,
+            status=max_retries,
+            status_forcelist=status_forcelist,
+            backoff_factor=backoff_factor,
+            allowed_methods=["HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS", "TRACE"],
+            raise_on_status=False,
+        )
+
+        adapter = HTTPAdapter(max_retries=retry_strategy)
+        self._session.mount("http://", adapter)
+        self._session.mount("https://", adapter)
+
 
     def _create_url(self, path: str):
         path_result = urlparse(path)
