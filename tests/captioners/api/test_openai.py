459fb94a5a691094ca3264a35ebe66b16f1b87e4
itterative <190138728+itterative@users.noreply.github.com>
2025-09-21 15:54:24 +0000

    text: openai o4-mini

--- tests/captioners/api/test_openai.py
+++ tests/captioners/api/test_openai.py
@@ -45,9 +45,9 @@ def koboldcpp(session: requests.Session, request_mocker: requests_mock.Adapter, 
 def openrouter(session: requests.Session, request_mocker: requests_mock.Adapter, load_test_data):
     def _openrouter(case: str, model: str, base_url: str = 'mock://openrouter.ai/api/v1'):
         request_mocker.register_uri('GET', f'{base_url}/credits', json={'data': {'total_credits': 1.0, 'total_usage': 0.1}})
-        request_mocker.register_uri('GET', f'{base_url}/models', json={'data': [{'id': model, 'object': 'model', 'owned_by': 'koboldcpp'}]})
+        request_mocker.register_uri('GET', f'{base_url}/models', json={'data': [{'id': model, 'object': 'model', 'owned_by': 'openrouter'}]})
 
-        request_mocker.register_uri('POST', f'{base_url}/chat/completions', text=load_test_data('openrouter_gpt_5_mini.txt'))
+        request_mocker.register_uri('POST', f'{base_url}/chat/completions', text=load_test_data(case))
 
         captioner = OpenAICaptioner(api_url=f'{base_url}', session=session)
 
@@ -55,7 +55,18 @@ def openrouter(session: requests.Session, request_mocker: requests_mock.Adapter,
 
     return _openrouter
 
+@pytest.fixture
+def openai(session: requests.Session, request_mocker: requests_mock.Adapter, load_test_data):
+    def _openrouter(case: str, model: str, base_url: str = 'mock://api.openai.com/v1'):
+        request_mocker.register_uri('GET', f'{base_url}/models', json={'data': [{'id': model, 'object': 'model', 'owned_by': 'openai'}]})
 
+        request_mocker.register_uri('POST', f'{base_url}/chat/completions', text=load_test_data(case))
+
+        captioner = OpenAICaptioner(api_url=f'{base_url}', session=session)
+
+        return captioner
+
+    return _openrouter
 
 def test_koboldcpp(koboldcpp, load_test_data):
     captioner: OpenAICaptioner = koboldcpp('koboldcpp.txt', 'koboldcpp/gemma-3-27b')
@@ -92,6 +103,21 @@ def test_openrouter_gpt_5_mini(openrouter, load_test_data):
 
 def test_openrouter_raises_error_on_bad_model(openrouter, load_test_data):
     captioner: OpenAICaptioner = openrouter('openrouter_gpt_5_mini.txt', 'openai/gpt-5-mini')
+
+    with pytest.raises(ValueError, match=re.compile('model not found: .*')):
+        captioner.load_model('unknown')
+
+def test_openai_o4_mini(openai, load_test_data):
+    captioner: OpenAICaptioner = openai('openai_o4_mini.txt', 'o4-mini')
+    captioner.load_model('o4-mini')
+
+    expected = load_test_data('openai_o4_mini_result.txt')
+    got = captioner.predict(mock.MagicMock(spec=DatasetImage))
+
+    assert got == expected, 'bad prediction'
+
+def test_openai_raises_error_on_bad_model(openai, load_test_data):
+    captioner: OpenAICaptioner = openai('openai_o4_mini.txt', 'o4-mini')
 
     with pytest.raises(ValueError, match=re.compile('model not found: .*')):
         captioner.load_model('unknown')
