2acc5b94db8814d98b26159a139b960cf56a413a
itterative <190138728+itterative@users.noreply.github.com>
2026-05-16 19:52:14 +0000

    refactor(cli): merge add/edit into edit for templates and configs

--- yadc/cli_configs.py
+++ yadc/cli_configs.py
@@ -34,38 +34,6 @@ def list():
         click.echo(template)
 
 @configs.command(
-    'add',
-    short_help='Add a new user config',
-    help='Add a new user config',
-)
-@click.argument('name', type=str)
-@cli_common.log_level
-def add(name: str):
-    try:
-        cmd_configs.load_user_config(name)
-
-        _logger.error('Error: config with name "%s" already exists. Use "configs edit" to edit it.', name)
-        sys.exit(cmd_status.STATUS_USER_ERROR)
-    except FileNotFoundError:
-        pass
-
-    default_template = '# Edit the template below\n\n'
-
-    user_template = click.edit(default_template, extension='.toml', require_save=True)
-
-    if user_template is None:
-        _logger.info('Cancelled.')
-        return
-
-    try:
-        cmd_configs.save_user_config(name, user_template)
-    except PermissionError:
-        _logger.error('Error: failed to save user config: permissions error')
-        sys.exit(cmd_status.STATUS_ERROR)
-
-    _logger.info('Added new user config: %s', name)
-
-@configs.command(
     'delete',
     short_help='Delete a user config',
     help='Delete an existing user config',
@@ -80,17 +48,17 @@ def delete(name: str):
 @configs.command(
     'edit',
     short_help='Edit a user config',
-    help='Edit an existing user config',
+    help='Edit an existing user config, or create a new one if it does not exist',
 )
 @click.argument('name', type=str)
 @cli_common.log_level
 def edit(name: str):
+    is_new = False
     try:
         user_template = cmd_configs.load_user_config(name)
     except FileNotFoundError:
-        _logger.error('Error: template with name "%s" does not exit. Use "configs add" to add it.', name)
-        sys.exit(cmd_status.STATUS_USER_ERROR)
-
+        is_new = True
+        user_template = '# Edit the template below\n\n'
 
     user_template = click.edit(user_template, extension='.toml', require_save=True)
 
@@ -104,4 +72,7 @@ def edit(name: str):
         _logger.error('Error: failed to save user config: permissions error')
         sys.exit(cmd_status.STATUS_ERROR)
 
-    _logger.info('Updated user config: %s', name)
+    if is_new:
+        _logger.info('Created new user config: %s', name)
+    else:
+        _logger.info('Updated user config: %s', name)
