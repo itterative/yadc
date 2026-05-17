783dc028667c82f5e8622f0f7af6c17ac0a42277
itterative <190138728+itterative@users.noreply.github.com>
2026-05-17 19:03:48 +0000

    fix: correct debug defaults

--- yadc/core/env.py
+++ yadc/core/env.py
@@ -1,4 +1,4 @@
 import os
 
-DEBUG_CAPTION_RESPONSES: bool = os.environ.get("YADC_DEBUG_CAPTION_RESPONSES", "").strip() == "1"
-DEBUG_CAPTION_REQUESTS_BODY: bool = os.environ.get("YADC_DEBUG_CAPTION_REQUESTS_BODY", "1").strip() == "1"
+DEBUG_CAPTION_RESPONSES: bool = os.environ.get("YADC_DEBUG_CAPTION_RESPONSES", "0").strip() == "1"
+DEBUG_CAPTION_REQUESTS_BODY: bool = os.environ.get("YADC_DEBUG_CAPTION_REQUESTS_BODY", "0").strip() == "1"
